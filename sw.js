3bce56cbe4d142b10ea7bdba25c09cdd31da6517
greenit205-hash <greenit205@gmail.com>
2026-08-24 08:54:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Offline dla aplikacji Jadłospis. Po każdej zmianie plików podnieś numer CACHE. */
-const CACHE = "jadlospis-v14";
+const CACHE = "jadlospis-v17";
 const CORE = ["./", "./index.html", "./manifest.json", "./icon.svg", "./icon-maskable.svg"];
 
 self.addEventListener("install", e => {
